80b35dd1bda579ca33d920ebaca78fb612edfb94
bkRahul <rahulsatya1068@gmail.com>
2020-01-20 00:59:25 +0530

    Updates

---
+++ precache-manifest.9c881e70811e0fa9bde8312cfe74b30a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6428d2836d0c4e047a1052c69298905f",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "6c9ebbf429fbc3033131",
+    "url": "/react-tutorial/static/css/main.54eb7e57.chunk.css"
+  },
+  {
+    "revision": "f11647d487cc0c161a65",
+    "url": "/react-tutorial/static/js/2.214c3807.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.214c3807.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c9ebbf429fbc3033131",
+    "url": "/react-tutorial/static/js/main.bfa6b488.chunk.js"
+  },
+  {
+    "revision": "08bcc4b7ded1a2e62c40",
+    "url": "/react-tutorial/static/js/runtime-main.4a42324e.js"
+  }
+]);
